7cf689bf737931d219e0ef81f9917b48bf0df545
Mark <mark@remarkablemark.org>
2021-08-05 00:00:47 -0400

    feat(app): handle 500 error

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,24 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+import type { ErrorRequestHandler } from 'express';
+
 import { isProduction } from './config';
 import indexRouter from './routes/index';
 
 const app = express();
 
+/**
+ * Middleware.
+ */
 app.use(logger(isProduction ? 'combined' : 'dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+/**
+ * Routes.
+ */
 app.use('/', indexRouter);
 
 /**
@@ -20,5 +28,14 @@ app.use('/', indexRouter);
 app.use((request, response) => {
   response.status(404).send('Not Found');
 });
+
+/**
+ * 500 Internal Server Error
+ */
+// eslint-disable-next-line no-unused-vars
+app.use(((error, request, response, next) => {
+  console.error(error.stack);
+  response.status(500).send('Internal Server Error');
+}) as ErrorRequestHandler);
 
 export default app;
