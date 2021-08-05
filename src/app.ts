c882ceae3016c95a207656d2a74a06e7e06edc45
Mark <mark@remarkablemark.org>
2021-08-05 11:25:28 -0400

    feat(app): enable cors for all requests

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import logger from 'morgan';
 
 import type { ErrorRequestHandler } from 'express';
@@ -17,6 +18,7 @@ app.use(logger(isProduction ? 'combined' : 'dev', { skip: () => isTest }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 /**
  * Routes.
