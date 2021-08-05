77ca835fadf60255bed19ce12a3c9ec6fbbabd8d
Mark <mark@remarkablemark.org>
2021-08-04 23:47:13 -0400

    feat: update morgan to use 'combined' logger in production

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import usersRouter from './routes/users';
 
 const app = express();
 
-app.use(logger(isProduction ? 'tiny' : 'dev'));
+app.use(logger(isProduction ? 'combined' : 'dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
